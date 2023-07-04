48b1ad8b1ab3b9f50f745c6c12ea090a3f263757
chickennuggetcurrysauce <22001095@myrp.edu.sg>
2023-07-04 23:40:05 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22001095/demodatabase/MainActivity.java
@@ -0,0 +1,89 @@
+package sg.edu.rp.c346.id22001095.demodatabase;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+    Button btnGetTasks;
+    TextView tvResults;
+    ListView lv;
+    ArrayAdapter aaTasks;
+    EditText etTask;
+    EditText etDate;
+    boolean asc = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks = findViewById(R.id.btnGetTasks);
+        tvResults = findViewById(R.id.tvResults);
+        lv = findViewById(R.id.lv);
+        etTask = findViewById(R.id.etTask);
+        etDate = findViewById(R.id.etDate);
+
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask(etTask.getText().toString(), etDate.getText().toString());
+
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                ArrayList<Task> tasks = db.getTasks(asc);
+
+
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+                asc = !asc;
+                aaTasks = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, tasks);
+                lv.setAdapter(aaTasks);
+            }
+        });
+
+
+    }
+}
